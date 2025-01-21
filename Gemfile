ee6d05e0755b15502a929d12551c38603c7efb8b
LJH <passby158@gmail.com>
2025-01-21 22:26:18 +0900

    Feat(#1): 유저 생성 로그인을 위한 email password 컬럼 추가

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem "puma", ">= 5.0"
 # gem "jbuilder"
 gem 'dotenv-rails', groups: [:development, :test]
 
+gem 'bcrypt', '~> 3.1.7'
 # Use Active Model has_secure_password [https://guides.rubyonrails.org/active_model_basics.html#securepassword]
 # gem "bcrypt", "~> 3.1.7"
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_21_125553) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_21_132523) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -47,6 +47,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_21_125553) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
+    t.string "password_digest"
   end
 
   add_foreign_key "reservations", "test_schedules", column: "test_schedules_id"
