c8c9afa426762e0ba44e960342232a17f7d9b374
LJH <passby158@gmail.com>
2025-01-21 22:35:32 +0900

    Feat(#1): jwt를 위한 user 테이블 수정

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_21_132523) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_21_132720) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -49,6 +49,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_21_132523) do
     t.datetime "updated_at", null: false
     t.string "email"
     t.string "password_digest"
+    t.string "role"
   end
 
   add_foreign_key "reservations", "test_schedules", column: "test_schedules_id"
