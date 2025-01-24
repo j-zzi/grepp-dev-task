587e4b07d80d0e3d1b5a84a381708a9af2e71dbd
LJH <passby158@gmail.com>
2025-01-24 18:18:57 +0900

    Feat(#2): strong parameter 자도 래핑에 맞춤

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -3,14 +3,15 @@ class AuthenticationController < ApplicationController
 
   # return auth token once user is authenticated
   def authenticate
+    permitted_params = auth_params
     auth_token =
-      AuthenticateUser.new(auth_params[:email], auth_params[:password]).call
+      AuthenticateUser.new(permitted_params[:email], permitted_params[:password]).call
     json_response(auth_token: auth_token)
   end
 
   private
 
   def auth_params
-    params.permit(:email, :password)
+    params[:authentication].permit(:email, :password)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   post 'signup', to: 'user#create'
+  post 'auth/login', to: 'authentication#authenticate'
 end
